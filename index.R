4504874beaeff96ae4dee7ac0f6c4669909827a7
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2019-12-31 10:52:50 -0800

    Made webpage an exact copy of the report

---
+++ index.R
@@ -0,0 +1,3 @@
+# Run this to create index file that turns into github webpage https://sbashevkin.github.io/Delta-Smelt-conditions-report
+
+file.copy(from="Delta-Smelt-conditions-report.html", to="index.html", overwrite = TRUE)
