7f593bd0316f782ef1bc6d935f9d41ef8bba9486
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-09-25 14:56:10 -0700

    Deprecating this repo for the deltareportr package and redirecting there.

--- index.R
+++
@@ -1,3 +0,0 @@
-# Run this to create index file that turns into github webpage https://sbashevkin.github.io/Delta-Smelt-conditions-report
-
-file.copy(from="Delta-Smelt-conditions-report.html", to="index.html", overwrite = TRUE)
